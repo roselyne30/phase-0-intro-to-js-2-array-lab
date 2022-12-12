af2e24555c33a3ced6b0d5019db0b070c382a5b0
roselynelutomia <roselyne.lutomia@student.moringaschool.com>
2022-12-12 19:33:49 +0300

    my completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats = ["Milo", "Otis","Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat() {
+    cats.splice(0,0, "Bob");
+}
+function destructivelyRemoveLastCat() {
+    cats.splice(-1);
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(allcats) {
+    return allcats = ([...cats, "Broom"]);
+}
+function prependCat(cats1) {
+    return cats1 = (["Arnold", ...cats]);
+}
+function removeLastCat(catslast) {
+    return catslast = (cats.slice(0,cats.length -1));
+}
+function removeFirstCat(catsfirst) {
+    return catsfirst = (cats.slice(1));
+}
